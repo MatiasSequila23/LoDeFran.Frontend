7dbf6360b39d0106acc08843ec067f9367bc6e98
MatiasSequila23 <martinmatiassequila@outlook.es>
2025-07-19 23:57:21 -0300

    Primera versión

--- LoDeFran.Frontend/obj/Debug/net8.0/LoDeFran.Frontend.AssemblyInfo.cs
+++ LoDeFran.Frontend/obj/Debug/net8.0/LoDeFran.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856531c6bb4190854df28de285964bc22ec67806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64be6dc5854243fc2dfd4d2eb44efdac66d700f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
