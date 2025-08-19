5bfb841eadac1b5dc0d2e51f73321d4e4e3cbd68
MatiasSequila23 <martinmatiassequila@outlook.es>
2025-08-19 02:56:25 -0300

    Print tickets cuenta

--- LoDeFran.Frontend/obj/Debug/net8.0/LoDeFran.Frontend.AssemblyInfo.cs
+++ LoDeFran.Frontend/obj/Debug/net8.0/LoDeFran.Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317f8284ee114936693727ec62b0a505a081ed10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6cfefafa5581d9dadd75ef768fbdfafc6f12180")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoDeFran.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
